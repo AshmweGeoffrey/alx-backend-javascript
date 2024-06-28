6a1d97aed04eb663726f08d23fc8daf840b0a1ec
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-06-28 22:55:07 +0200

    22:55

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,11 @@
-import ClassRoom from './0-classroom';
+import Classroom from './0-classroom';
 
-export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
 }
+
+export default initializeRooms;
